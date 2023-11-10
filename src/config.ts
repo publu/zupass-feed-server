1618759e27c95ca81120fc091a787eeda531904d
Pablo The Penguin <pablo12335@gmail.com>
2023-11-10 12:13:29 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -33,7 +33,7 @@ export async function loadTickets(): Promise<Record<string, Ticket[]>> {
         "ticketId": "f65d8af8-e4c8-41c1-b9e2-0fb5d197c2ba", 
         "eventId": "3e8970cf-499b-4679-967b-8aa6647b288e",  
         "productId": "a9f5e8a9-5a6e-4419-aa80-5c0f18efb6dd",
-        "ticketCategory": "ZuConnect"
+        "ticketCategory": "HackZuzalu"
       }
     ]
   });
